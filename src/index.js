04b48b090d08d6db7cc464ae6190f8a79d0b677c
Asvarox <asvarox93@gmail.com>
2020-04-28 19:51:37 +0200

    Install Apollo Client and Connect to DB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  gql,
+  ApolloProvider,
+} from "@apollo/client";
 import "./index.css";
 import App from "./App";
 
+const URL = "https://examples.devmastery.pl/random-stuff/graphql";
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({ uri: URL }),
+});
+
+const RANDOM_QUOTE_QUERY = gql`
+  query getRandomQuote {
+    randomQuote {
+      text
+      author
+    }
+  }
+`;
+
+client
+  .query({ query: RANDOM_QUOTE_QUERY })
+  .then((result) => console.log("query result:", result.data));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
